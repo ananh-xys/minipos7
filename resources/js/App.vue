762868874243917d90f13d441b1f112cb86ebc2e
ananh-xys <ananh.xys@gmail.com>
2023-02-06 23:58:02 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h1>hello VUE JS</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos7App',
+
+    data() {
+        return {
+            
+        };
+    },
+
+
+    mounted() {
+        
+    },
+    components:{
+
+    },
+
+    computed:{
+
+    },
+
+    methods: {
+        
+    },
+
+   
+    watch(){
+
+    },
+
+    create(){
+        
+    },
+    
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
